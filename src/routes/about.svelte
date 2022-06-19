b5a39d2bfd35506729835b656934c2202e4faf5d
hsukiasyan <h.sukiasyan@gethatch.com>
2022-06-19 11:44:32 +0200

    initial setup

---
+++ src/routes/about.svelte
@@ -0,0 +1,5 @@
+<svelte:head>
+	<title>About Page</title>
+</svelte:head>
+
+<h1>This is about</h1>
---
+++ src/routes/index.svelte
@@ -0,0 +1,8 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
+<h1>Welcome to SvelteKit</h1>
